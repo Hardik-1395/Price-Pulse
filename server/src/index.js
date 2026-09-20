c63373fa4959b42d9549aa4f14e6661e36fc8622
Hardik Gupta <kumarnaveen6248@gmail.com>
2026-09-20 22:39:53 +0530

    index fix

--- server/src/index.js
+++ server/src/index.js
@@ -268,5 +268,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-    console.log("Server running on http://localhost:3000");
-});
+
